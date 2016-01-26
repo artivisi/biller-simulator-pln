1db2348c857345f63fa882300f4447bf2f768720
Endy Muhardin <endy.muhardin@gmail.com>
2016-01-26 10:32:57 +0700

    starter project

---
+++ simulator-pln/src/main/java/com/artivisi/biller/simulator/SimulatorPlnApplication.java
@@ -0,0 +1,12 @@
+package com.artivisi.biller.simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulatorPlnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulatorPlnApplication.class, args);
+	}
+}
---
+++ simulator-pln/src/test/java/com/artivisi/biller/simulator/SimulatorPlnApplicationTests.java
@@ -0,0 +1,18 @@
+package com.artivisi.biller.simulator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SimulatorPlnApplication.class)
+@WebAppConfiguration
+public class SimulatorPlnApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
